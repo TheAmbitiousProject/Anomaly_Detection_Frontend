a59d882b2bdc2562dca4e8574b8437dc2b466283
nayana-vinod <nayanavinodk33@gmail.com>
2023-03-19 00:51:13 +0530

    landing page and signin MUI

---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,38 @@
+import type { NextPage } from 'next'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import Account from '../components/Account'
+import Footer from '../components/Footer'
+import { height } from '@mui/system'
+
+const LandingPage: NextPage = () => {
+  const session = useSession()
+  const supabase = useSupabaseClient()
+
+  return (
+    <div className="container">
+      {!session ? (
+        <div className="row flex flex-row flex-center items-center w-screen h-screen">
+          <div className="basis-3/5">
+            <h1 className="header text-5xl text-white">Anomaly Detection</h1>
+            <p className="text-center text-white">
+            protecting people from all anomalies at all time..
+            </p>
+          </div>
+          <div className="auth-widget basis-2/5">
+            <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark" />
+          </div>
+        </div>
+      ) : (
+        <>
+          <Account session={session} />
+        </>
+      )}
+
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
